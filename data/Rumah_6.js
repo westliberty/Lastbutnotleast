26c47c2418cd21acbcc71911024570189a3bc504
RKO <112848307+westliberty@users.noreply.github.com>
2022-12-09 16:24:05 +0700

    Add files via upload

---
+++ data/Rumah_6.js
@@ -0,0 +1,1 @@
+var json_Rumah_6 = {"type":"FeatureCollection","name":"Rumah_6","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[110.410827317119626,-7.810313132342507]}}]}
